b2b9cbc04c8103f8b8863efaa4786116336e3940
arbas9208 <arbas9208@7L48282>
2025-12-18 09:49:03 -0500

    Test1

--- DinoDashFinalProject/src/dinodashfinalproject/DinoDashFinalProject.java
+++ DinoDashFinalProject/src/dinodashfinalproject/DinoDashFinalProject.java
@@ -9,6 +9,7 @@ public class DinoDashFinalProject {
     public static void main(String[] args) {
         System.out.println("DinoDash");
         System.out.println("Gernig");
+        System.out.println("Test1");
     }
 
 }
