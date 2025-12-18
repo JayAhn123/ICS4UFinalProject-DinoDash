02f39cb7894ff1fb55b2da96373a5e940b73b045
JaAhn8882 <JaAhn8882@8D1X182>
2025-12-18 09:41:45 -0500

    First Commit

---
+++ DinoDashFinalProject/src/dinodashfinalproject/DinoDashFinalProject.java
@@ -0,0 +1,13 @@
+package dinodashfinalproject;
+
+/**
+ *
+ * @author JaAhn8882
+ */
+public class DinoDashFinalProject {
+
+    public static void main(String[] args) {
+        System.out.println("DinoDash");
+    }
+
+}
