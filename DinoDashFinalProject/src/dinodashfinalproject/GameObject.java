77529ef5525367601425ed0ba70d1554632b3ac6
JaAhn8882 <JaAhn8882@BDLY082>
2025-12-19 09:51:42 -0500

    Almost Finished GameObject Class

--- DinoDashFinalProject/src/dinodashfinalproject/GameObject.java
+++ DinoDashFinalProject/src/dinodashfinalproject/GameObject.java
@@ -1,16 +1,16 @@
 package dinodashfinalproject;
 
-/**
- *
- * @author JaAhn8882
+/* Jay Ahn
+ * Dec 19, 2025
+ * GameObject Class for Dino Dash
  */
 public class GameObject {
 
     //attributes
-    int x;
-    int y;
-    int width;
-    int height;
+    int x;//x position of the game object
+    int y;//y position of the game object
+    int width;//width of the game object
+    int height;//height of the game object
 
     /**
      * Primary Constructor that instantiates game object
@@ -39,6 +39,102 @@ public class GameObject {
         this.width = width;
         this.height = height;
     }
-    
-    public void 
+
+    //Getters
+    /**
+     * gets x position of the game object
+     *
+     * @return - x position of the game object
+     */
+    public int getX() {
+        return x;//returns x position
+    }
+
+    /**
+     * gets y position of the game object
+     *
+     * @return - y position of the game object
+     */
+    public int getY() {
+        return y;//returns y position
+    }
+
+    /**
+     * gets width of the game object
+     *
+     * @return - width of the game object
+     */
+    public int getWidth() {
+        return width;//returns width
+    }
+
+    /**
+     * gets height of the game object
+     *
+     * @return - height of the game object
+     */
+    public int getHeight() {
+        return height;//returns height
+    }
+
+    //Setters
+    /**
+     * sets x position of the game object
+     *
+     * @param x - new x position
+     */
+    public void setX(int x) {
+        this.x = x;//sets x position
+    }
+
+    /**
+     * sets y position of the game object
+     *
+     * @param y - new y position
+     */
+    public void setY(int y) {
+        this.y = y;//sets y position
+    }
+
+    /**
+     * sets width of the game object
+     *
+     * @param width - new width
+     */
+    public void setWidth(int width) {
+        this.width = width;//sets width
+    }
+
+    /**
+     * sets height of the game object
+     *
+     * @param height - new height
+     */
+    public void setHeight(int height) {
+        this.height = height;//sets height
+    }
+
+    /**
+     * determines if other game object is equal to current game object
+     *
+     * @param other - other game object that is being compared
+     * @return - boolean; true if they are equal, false if they are not equal
+     */
+    public boolean equals(GameObject other) {
+        //if attributes of current game object and other game object is equal
+        if (this.x == other.x && this.y == other.x && this.width == other.width && this.height == other.height) {
+            return true;//return true
+        } else {//else
+            return false;//return false
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public GameObject clone() {
+
+    }
+
 }
