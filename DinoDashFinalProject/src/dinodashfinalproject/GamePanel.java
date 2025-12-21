72d42b6c8bd2548c25c540fa7f3d8046b2d8d6ad
AraibBasit <>
2025-12-20 20:10:50 -0500

    setup so that it now runs at 50fps and can be animated because of threads

--- DinoDashFinalProject/src/dinodashfinalproject/GamePanel.java
+++ DinoDashFinalProject/src/dinodashfinalproject/GamePanel.java
@@ -9,31 +9,35 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author arbas9208
  */
-public class GamePanel extends JPanel{
-    
+public class GamePanel extends JPanel implements Runnable {
+
+    //global variables
+    private Thread animator;
+    private final int DELAY = 20;
+
     /**
      * Does the actual drawing
      *
      * @param g - the Graphics object to draw with
      */
     private void doDrawing(Graphics g) {
-        
+
         //the Graphics2D class is the class that handles all the drawing
         //must be casted from older Graphics class in order to have access to some newer methods
         Graphics2D g2d = (Graphics2D) g;
         //set background colour
         g2d.setColor(Color.CYAN);
-        g2d.fillRect(0,0,this.getWidth(),this.getHeight());
+        g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
         //draw Game title
         g2d.setColor(Color.black);
-        g2d.drawString("Dino Dash",350,300);
+        g2d.drawString("Dino Dash", 350, 300);
 
-        
     }
 
     /**
@@ -43,5 +47,59 @@ public class GamePanel extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);//does the necessary work to prepare the panel for drawing
         doDrawing(g); //invoke our custom drawing method
+    }
+
+    /**
+     * this method is called after the JPanel is added to the JFrame we can
+     * perform start up tasks here
+     *
+     * @Override
+     */
+    public void addNotify() {
+        super.addNotify();
+
+        animator = new Thread(this);
+        animator.start();
+    }
+
+    /**
+     * this method is called only once, when the Thread starts
+     *
+     * @Override
+     */
+    public void run() {
+
+        //setup variables for timing
+        long beforeTime, timeDiff, sleep;
+        //get the current time
+        beforeTime = System.currentTimeMillis();
+
+        while (true) { //this loop runs once every 20 ms (the DELAY) shloud be 50 fps
+
+            //redraws the screen (calling the paint component method)
+            repaint();
+
+            //calculate how much time has passed since the last call
+            //this allows smooth updates and our ball will move at a constant speed (as opposed to being dependent on processor availability)
+            timeDiff = System.currentTimeMillis() - beforeTime;
+
+            //calculate how much time to wait before the next call
+            sleep = DELAY - timeDiff;
+
+            //always wait at least 2 ms
+            if (sleep < 0) {
+                sleep = 2;
+            }
+
+            //try to actually wait
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                //threads can be interrupted from other threads
+                JOptionPane.showMessageDialog(this, "Thread interrupted: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            //get the new current time
+            beforeTime = System.currentTimeMillis();
+        }
     }
 }
